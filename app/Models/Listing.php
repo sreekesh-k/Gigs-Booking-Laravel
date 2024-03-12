2f755bba28721e78c329dda363a8af0d4b4efc9f
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-12 18:01:32 +0530

    model

---
+++ app/Models/Listing.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+class Listing
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus aliquid esse molestiae rem ipsa eius facere consequatur rerum! Cumque tempora maxime dicta voluptatem perferendis ad numquam blanditiis quas adipisci ea?'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus aliquid esse molestiae rem ipsa eius facere consequatur rerum! Cumque tempora maxime dicta voluptatem perferendis ad numquam blanditiis quas adipisci ea?'
+            ]
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('listings', [
         'heading'  => 'Latest Listings',
-        'listings' => [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus aliquid esse molestiae rem ipsa eius facere consequatur rerum! Cumque tempora maxime dicta voluptatem perferendis ad numquam blanditiis quas adipisci ea?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus aliquid esse molestiae rem ipsa eius facere consequatur rerum! Cumque tempora maxime dicta voluptatem perferendis ad numquam blanditiis quas adipisci ea?'
-            ]
-        ]
+        'listings' => Listing::all()
     ]);
 });

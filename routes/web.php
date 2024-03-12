4f88b34c0c79b65d3cd0eaf7340f5f9392993d4a
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-12 15:59:16 +0530

    return response string

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function(){
+    return response('<h1>HELLO WORLD</h1>');
+});
+

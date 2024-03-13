59fbbba191324e2c1e93cbc38fa0f69b5b22856b
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-13 11:37:22 +0530

    layout implementation

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,10 @@
+@extends('layout')
+
+@section('content')
+    
 <h1>{{ $heading }}</h1>
 @foreach ($listings as $listing)
     <h2><a href = "/listings/{{$listing['id']}}">{{ $listing['title'] }}</a></h2>
     <p>{{ $listing['description'] }}</p>
 @endforeach
+@endsection

ed9b8de21bf45c4a70a97ff40da5d2891f76ca78
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-13 12:33:15 +0530

    components

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,5 +1,5 @@
 @props(['listing'])
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
         <div>
@@ -26,4 +26,4 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,7 +5,7 @@
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
@@ -36,18 +36,18 @@
                     <div class="text-lg space-y-6">
                         {{ $listing->description }}
 
-                        <a href="mailto:{{$listing->email}}"
+                        <a href="mailto:{{ $listing->email }}"
                             class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
                                 class="fa-solid fa-envelope"></i>
                             Contact Employer</a>
 
-                        <a href="{{$listing->website}}" target="_blank"
+                        <a href="{{ $listing->website }}" target="_blank"
                             class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i
                                 class="fa-solid fa-globe"></i> Visit
                             Website</a>
                     </div>
                 </div>
             </div>
-        </div>
+        </x-card>
     </div>
 @endsection

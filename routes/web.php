af4fabf2f38620f022551bb94eaa2f734f3961cc
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-13 12:09:29 +0530

    rout 404 page

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,13 @@ Route::get('/', function () {
 
 //list id
 Route::get('/listings/{id}',function($id){
-    return view('listing',[
-        'listing'=>Listing::find($id)
-    ]);
+    $listing = Listing::find($id);
+    if($listing){
+        return view('listing',[
+            'listing'=>$listing
+        ]);
+    }
+    else{
+        abort('404');
+    }
 });

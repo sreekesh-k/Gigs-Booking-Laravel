df3a0475cf6cef7ae9b17d69d48975fc8984f970
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-13 11:24:58 +0530

    created model and seeding

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,29 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus aliquid esse molestiae rem ipsa eius facere consequatur rerum! Cumque tempora maxime dicta voluptatem perferendis ad numquam blanditiis quas adipisci ea?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus aliquid esse molestiae rem ipsa eius facere consequatur rerum! Cumque tempora maxime dicta voluptatem perferendis ad numquam blanditiis quas adipisci ea?'
-            ]
-        ];
-    }
-    public static function find($id){
-        $listings = self::all();
-        foreach($listings as $listing){
-            if($listing['id'] ==$id){
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
